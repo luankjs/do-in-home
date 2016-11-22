6c6d3fa7103cd4f85cbd8771665b84435f3f1bcc
Luan Santos <luankjs@gmail.com>
2016-11-22 20:47:57 -0300

    Adds initial configuraiton

--- config/application.rb
+++ config/application.rb
@@ -3,12 +3,12 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "action_cable/engine"
+# require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -18,8 +18,7 @@ Bundler.require(*Rails.groups)
 
 module DoInHome
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Brasilia'
+    config.i18n.default_locale = 'pt-BR'
   end
 end
