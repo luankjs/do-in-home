bb706909ebe1cee0c8de1e9009bc83f9a4a1c600
Luan Santos <luankjs@gmail.com>
2016-11-24 20:11:56 -0300

    Adds model tests to Score

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,8 @@
 class Score < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  validates :points, presence: true, numericality: { only_integer: true }
+  validates :user_id, presence: true
+  validates :task_id, presence: true
 end
