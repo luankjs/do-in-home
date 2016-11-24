ffff40db082f4db7454b10f410ccf65f7574234c
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-11-24 01:13:52 -0300

    Score model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124023717) do
+ActiveRecord::Schema.define(version: 20161124040825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "scores", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "task_id"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_scores_on_task_id", using: :btree
+    t.index ["user_id"], name: "index_scores_on_user_id", using: :btree
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -48,5 +58,7 @@ ActiveRecord::Schema.define(version: 20161124023717) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "scores", "tasks"
+  add_foreign_key "scores", "users"
   add_foreign_key "tasks", "users"
 end
---
+++ spec/factories/scores.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :score do
+    user nil
+    task nil
+    score 1
+  end
+end
