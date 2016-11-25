94a57eeb06349b3973ff75a5eefaad6cbc013422
Hamurabi Araújo <araujohamurabi@gmail.com>
2016-11-24 22:02:27 -0300

    Score ranking created

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,5 +1,6 @@
 class ScoreController < ApplicationController
   def index
-    @scores = Score.all
+    # @scores = Score.all(:group => "user_id", :order => "created_at")
+    @scores = Score.group("user_id").select("user_id, SUM(points) as points").order("points DESC")
   end
 end
